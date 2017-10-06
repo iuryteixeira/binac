64b781adfa2a3744288747e3c3d5e21873c9243d
Iury Teixeira <franciury@gmail.com>
2017-10-06 01:17:17 -0300

    change number base

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,40 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  selectedOption: string;
+  ipNumber: string;
+  labelField: string = "Número";
+  inpState: boolean = true;
+
   constructor(public navCtrl: NavController) {
 
   }
+
+  /**
+   * response to options of select
+   */
+  selectClick() {
+    this.changeLabel();
+    this.inpState = false;
+  }
+
+  /**
+   * change field label with base in the selection
+   */
+  changeLabel() {
+    if (this.selectedOption == 'bin') {
+      this.labelField = "Insira o binário";
+    } 
+    if (this.selectedOption == 'oct') {
+      this.labelField = "Insira o octal";
+    }
+    if (this.selectedOption == 'dec') {
+      this.labelField = "Insira o decimal";
+    }
+    if (this.selectedOption == 'hex') {
+      this.labelField = "Insira o hexa";
+    }
+  }
+
 
 }
